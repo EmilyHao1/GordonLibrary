fe0e52f122e55b74826bcb820d5f6afaf93dfe20
loriluo95 <zluo2@wpi.edu>
2017-10-04 14:53:23 -0400

    set connection to pg-loriluo95

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
-
+const pg = require('pg');
 const app = express();
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening at ${port}`);
+});
+
+var SQLconnection =
+  'postgres://xkvvjoiw:O6_92MrJi1A2YxAtGWFCO2PrqU3oJdGn@stampy.db.elephantsql.com:5432/xkvvjoiw';
+var client = new pg.Client(SQLconnection);
+client.connect();
+
+var query = client.query('SELECT * From Books', function(err, result) {
+  result.rows.forEach(function(row) {
+    console.log(row.bookid);
+  });
 });
