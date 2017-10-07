dfdbe482ce27762c54d2f6f81b39ebfa19c346a6
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-10-07 13:08:21 -0400

    feat: Add route to /books to query all books in database

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
+const path = require('path');
 const pg = require('pg');
+
+const db = require('./db')
+
 const app = express();
+
+// static routes
+app.use(express.static(path.join(__dirname, 'public')));
+// routes
+app.get('/books', (req, res) => {
+  db.client.query('SELECT * From books', (err, result) => {
+    res.send(JSON.stringify(result.rows));
+  });
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening at ${port}`);
-});
-
-var SQLconnection =
-  'postgres://xkvvjoiw:O6_92MrJi1A2YxAtGWFCO2PrqU3oJdGn@stampy.db.elephantsql.com:5432/xkvvjoiw';
-var client = new pg.Client(SQLconnection);
-client.connect();
-
-client.query('SELECT * From books', function(err, result) {
-  console.log('Bookid, ISBN, Title, Authors');
-  result.rows.forEach(function(row) {
-    console.log(
-      row.bookid + ',' + row.isbn + ',' + row.title + ',' + row.authors
-    );
-  });
 });
