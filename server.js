672e1446549beec24d82c3d22712fd460d8615cf
ming xu <mingxu@mings-MacBook-Pro.local>
2017-10-07 14:30:09 -0400

    create login page html

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
-const express = require('express');
-const pg = require('pg');
-const app = express();
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`listening at ${port}`);
-});
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
 
-var SQLconnection =
-  'postgres://xkvvjoiw:O6_92MrJi1A2YxAtGWFCO2PrqU3oJdGn@stampy.db.elephantsql.com:5432/xkvvjoiw';
-var client = new pg.Client(SQLconnection);
-client.connect();
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'public/indexLogin.html')
+      break
+    case '/indexLogin.html':
+      sendFile(res, 'public/indexLogin.html')
+      break
+    case '/indexDashboard.html':
+      sendFile(res, 'public/indexDashboard.html')
+      break
+    case '/indexBookSearch.html':
+      sendFile(res, 'public/indexBookSearch.html')
+      break
+    case '/css/style.css':
+      sendFile(res, 'public/css/style.css', 'text/css')
+      break
+    case '/js/scripts.js':
+      sendFile(res, 'public/js/scripts.js', 'text/javascript')
+      break
+    default:
+      res.end('404 not found')
+  }
+})
 
-client.query('SELECT * From books', function(err, result) {
-  console.log('Bookid, ISBN, Title, Authors');
-  result.rows.forEach(function(row) {
-    console.log(
-      row.bookid + ',' + row.isbn + ',' + row.title + ',' + row.authors
-    );
-  });
-});
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename, contentType) {
+  contentType = contentType || 'text/html';
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': contentType})
+    res.end(content, 'utf-8')
+  })
+
+}
