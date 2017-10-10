b5a2f440973a275862eefd5d4b8f460e47c38ad3
Emily Hao <Emilyhaomac@autoreg-704533.wifi.wpi.edu>
2017-10-10 01:01:42 -0400

    index reserve tech suite

--- server.js
+++ server.js
@@ -1,18 +1,68 @@
-const express = require('express');
-const path = require('path');
-const pg = require('pg');
+// const express = require('express');
+// const path = require('path');
+// const pg = require('pg');
+//
+// const db = require('./db');
+// const book = require('./routes/book');
+//
+// const app = express();
+//
+// // static routes
+// app.use(express.static(path.join(__dirname, 'public')));
+// // routes
+// app.use('/book', book);
+//
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//   console.log(`listening at ${port}`);
+// });
 
-const db = require('./db');
-const book = require('./routes/book');
+var http = require('http')
+    , fs   = require('fs')
+    , url  = require('url')
+    , port = 8080;
 
-const app = express();
+var server = http.createServer (function (req, res) {
+    var uri = url.parse(req.url)
 
-// static routes
-app.use(express.static(path.join(__dirname, 'public')));
-// routes
-app.use('/book', book);
+    switch( uri.pathname ) {
+        case '/':
+            sendFile(res, 'public/indexLogin.html')
+            break
+        case '/indexLogin.html':
+            sendFile(res, 'public/indexLogin.html')
+            break
+        case '/indexDashboard.html':
+            sendFile(res, 'public/indexDashboard.html')
+            break
+        case '/indexBookSearch.html':
+            sendFile(res, 'public/indexBookSearch.html')
+            break
+        case '/indexReserveTechSuite.html':
+            sendFile(res, 'public/indexReserveTechSuite.html')
+            break
+        case '/css/style.css':
+            sendFile(res, 'public/css/style.css', 'text/css')
+            break
+        case '/js/scripts.js':
+            sendFile(res, 'public/js/scripts.js', 'text/javascript')
+            break
+        default:
+            res.end('404 not found')
+    }
+})
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`listening at ${port}`);
-});
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename, contentType) {
+    contentType = contentType || 'text/html';
+
+    fs.readFile(filename, function(error, content) {
+        res.writeHead(200, {'Content-type': contentType})
+        res.end(content, 'utf-8')
+    })
+
+}
